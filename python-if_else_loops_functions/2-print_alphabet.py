3ca373f5bc4c92e5fd3c90e5268344a03d449dda
psychohight <7298@holbertonstudents.com>
2024-05-09 20:17:49 +0200

    2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-
-print(''.join(chr(letter) for letter in range(97, 123) if letter != 101 and letter != 113), end='')
+print(''.join(chr(letter) for letter in range(97, 123) if letter != 101 and letter != 113).format(), end='')
