dbfcb68d6976e5b44f70806b3bf3a743c51c2f62
psychohight <7298@holbertonstudents.com>
2024-05-10 12:17:23 +0200

    5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(100):
     if i < 99:
-        print("{}, ".format(i), end="")
+        print("{:02}, ".format(i), end="")
     else:
-        print("{}".format(i))
+        print("{:02}".format(i))
