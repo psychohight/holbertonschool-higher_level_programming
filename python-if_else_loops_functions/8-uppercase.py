98780b3c9e255a81d0af83085b6332f700a95001
psychohight <7298@holbertonstudents.com>
2024-05-10 16:56:14 +0200

    9

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    for c in str:
-        if 'a' <= c <= 'z':
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
+    for alph  in str:
+        if 'a' <= alph <= 'z':
+            c = chr(ord(alph) - 32)
+        print("{}".format(alph), end="")
     print()
