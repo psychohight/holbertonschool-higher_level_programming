7c40a568173dbcac3e59b23512af41dc4a50cd3a
psychohight <7298@holbertonstudents.com>
2024-05-11 18:31:06 +0200

    1-2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def print_arguments():
+    args = sys.argv[1:]
+    num_args = len(args)
+    
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+    
+    for i, arg in enumerate(args, 1):
+        print("{}: {}".format(i, arg))
+
+if __name__ == "__main__":
+    print_arguments()
---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def calcul_infinite_add():
+    
+
