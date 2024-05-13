dba9df556065130c58371fc337c08d0784fa37ab
psychohight <7298@holbertonstudents.com>
2024-05-13 17:16:57 +0200

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(len(i)):
+            if j == len(i) - 1:
+                print("{:d}".format(i[j]))
+            else:
+                print("{:d}".format(i[j]), end=" ")
