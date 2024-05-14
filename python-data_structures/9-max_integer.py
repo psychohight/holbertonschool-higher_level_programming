50f853bf1f42fcbb9d328aa1826206c691fd6a61
psychohight <7298@holbertonstudents.com>
2024-05-14 14:47:11 +0200

    9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    
+    max_a = my_list[0]
+    for i in my_list:
+        if i > max_a:
+            max_a = i
+
+    return max_a
