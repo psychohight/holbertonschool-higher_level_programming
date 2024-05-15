1736f1c76d8f6b530b63e47adb2a3777bd56be07
psychohight <7298@holbertonstudents.com>
2024-05-15 17:16:25 +0200

    2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    uniq_nbr = set(my_list) # set: tous les doublons,triplés,ect.. sont automatiquement éliminés
+    uniq_nbr = set(my_list)# set: tous les doublons,triplés,ect.. sont automatiquement éliminés
 
     total = sum(uniq_nbr)
 
