3c00f0842dd856ff28c5d1623fb8915dff6be41b
psychohight <7298@holbertonstudents.com>
2024-05-17 19:25:15 +0200

    3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,9 +4,8 @@ def safe_print_division(a, b):
 
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         result = None
-
     finally:
         print("inside result: {}".format(result))
         return result
