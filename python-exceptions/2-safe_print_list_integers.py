6e1c7ddf337c779fd8576df3d4a3584e1d4e1e04
psychohight <7298@holbertonstudents.com>
2024-05-17 19:21:09 +0200

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,16 +2,14 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    integ_count = 0
 
-    while count < x:
+    for i in range(x):
         try:
             print("{:d}".format(my_list[count]), end="")
-            integ_count += 1
             
         except (ValueError, TypeError):
             pass
         count += 1
         
     print()
-    return integ_count
+    return count
