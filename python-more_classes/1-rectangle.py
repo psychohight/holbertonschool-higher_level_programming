b498d011983bcd74715bc57c9ec41336b3bea312
psychohight <7298@holbertonstudents.com>
2024-05-23 15:04:16 +0200

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
+"""class rectangle
+"""
 
 class Rectangle:
+    """create a private instance
+        
+        width: horizontal 
+        height: vertical 
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """__width
+        Returns: __width
+        """
         return  self._width
     
     @width.setter
     def width(self, value):
+        """value: horizontal value
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +31,15 @@ class Rectangle:
 
     @property
     def height(self):
+        """__height
+        Returns: __height
+        """
         return self._height
     
     @height.setter
     def height(self, value):
+        """value: vertical value
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
