8102ce1086ca9a8140f351db5136931b6801cb7e
psychohight <7298@holbertonstudents.com>
2024-05-29 15:58:03 +0200

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it
     """
-    with open(filename, encoding='utf-8') as file:
-        print(file.read(), end="")
+    with open(filename, "r", encoding='utf-8') as file:
+        content = file.read()
+        print(content, end="")
