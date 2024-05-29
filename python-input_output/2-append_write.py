23d79e9ef39ae510790d3380cc054d571c90c665
psychohight <7298@holbertonstudents.com>
2024-05-29 15:54:52 +0200

    0-1-2-3-4-5-6

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF8) and returns the number of characters added
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file
+    """
+    with open(filename, mode='r', encoding='utf-8') as file:
+        return json.load(file)
