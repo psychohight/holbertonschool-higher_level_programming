1d989c089a5b2c03803b3bedb3f276daf46d93bb
psychohight <7298@holbertonstudents.com>
2024-06-21 16:35:11 +0200

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,16 @@
+#!/usr/bin/python3
 """
 lists all states from the database hbtn_0e_0_usa
 """
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
