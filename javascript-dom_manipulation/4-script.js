3d473b7f97530c3a013ab7caa6d44d28e49d90f5
psychohight <7298@holbertonstudents.com>
2024-07-26 15:44:56 +0200

    3 a 8

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('add_item').addEventListener('click', function() {
+      const newItem = document.createElement('li');
+      newItem.textContent = 'Item';
+      document.querySelector('.my_list').appendChild(newItem);
+    });
+  });
+  
---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('update_header').addEventListener('click', function() {
+      document.querySelector('header').textContent = 'New Header!!!';
+    });
+  });
+  
